f2330e80d570f7826db1dc9b7abbdba967fe100b
MThanmayee <thanmayee.matha@gmail.com>
2021-09-28 11:25:10 +0530

    account

---
+++ src/app/account.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from "./login/login.component";
+
+export class Account {
+    AccountNumber?:number;
+    CustomerId?:number;
+    AccountType?:string;
+    Balance?:number;
+    UserName?:string;
+    Email?:string;
+    Password?:string;
+    TransactionPassword?:string;
+
+
+
+}
